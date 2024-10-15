3743f654ac93a96f7c947101d659084f79a93964
kennjenga <kinyagia10@gmail.com>
2024-10-15 13:41:55 +0300

    Add async_comprehension function to generate a list of floats using async comprehensions

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -15,3 +15,5 @@ async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+
+# print(asyncio.run(wait_random(6)))
